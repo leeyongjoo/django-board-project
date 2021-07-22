93e3883aa63ccd1fb9edd7c23fe4a692d1834bc8
leeyongjoo <2yongjooo@gmail.com>
2021-07-22 11:32:34 +0900

    add modify or delete question, answer

--- board/models.py
+++ board/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+# TODO: date 필드 수정
+#  auto_add, auto_add_now 추가하기
 
 # Create your models here.
 class Question(models.Model):
@@ -8,6 +10,7 @@ class Question(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.subject
@@ -18,3 +21,4 @@ class Answer(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    modify_date = models.DateTimeField(null=True, blank=True)
