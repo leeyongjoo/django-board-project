b2c4822c7d26d45e4cf623e984ee26ddbac5735b
leeyongjoo <2yongjooo@gmail.com>
2021-07-24 20:30:20 +0900

    Feat: Add STATIC_ROOT

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,3 +1,7 @@
 from .base import *
 
 ALLOWED_HOSTS = []
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,7 @@
 from .base import *
 
 ALLOWED_HOSTS = ['13.209.108.62']
+
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+]
