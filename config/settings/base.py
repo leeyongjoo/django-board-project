af588a33704e086683d4ba57a1ace7ce0b781ba1
leeyongjoo <2yongjooo@gmail.com>
2021-07-26 16:42:05 +0900

    Style: rename log file
    저장되는 로그 파일 이름 변경

--- config/settings/base.py
+++ config/settings/base.py
@@ -172,7 +172,7 @@ LOGGING = {
             'level': 'INFO',
             'filters': ['require_debug_false'],
             'class': 'logging.handlers.RotatingFileHandler',
-            'filename': BASE_DIR / 'logs/mysite.log',
+            'filename': BASE_DIR / 'logs/django-board-practice.log',
             'maxBytes': 1024 * 1024 * 5,  # 5 MB
             'backupCount': 5,
             'formatter': 'standard',
