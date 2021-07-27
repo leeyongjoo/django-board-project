b208c07a6921aba679c7cd0c8a5de1370b91c859
leeyongjoo <2yongjooo@gmail.com>
2021-07-27 14:18:10 +0900

    Feat: Add sub domain
    서브 도메인 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['13.209.108.62', 'leeyongjoo.site']
+ALLOWED_HOSTS = ['13.209.108.62', 'board.leeyongjoo.site']
 
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
