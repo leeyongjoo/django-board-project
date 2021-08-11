2f546c93d705e917168249d43e9210b0c6a4fd7e
leeyongjoo <2yongjooo@gmail.com>
2021-08-11 22:30:59 +0900

    Style: add age param
    age 파라미터 추가

--- common/decorators.py
+++ common/decorators.py
@@ -3,7 +3,7 @@ from functools import wraps
 KEEP_LOGIN_AGE = 60 * 60 * 24 * 365  # 1 year
 
 
-def keep_login(view_func):
+def keep_login(view_func, age=KEEP_LOGIN_AGE):
     @wraps(view_func)
     def _wrapped_view_func(request, *args, **kwargs):
         response = view_func(request, *args, **kwargs)
@@ -11,7 +11,7 @@ def keep_login(view_func):
         # 로그인 성공 시 세션 만료 시간 설정
         if request.user.is_authenticated:
             if request.POST.get('keepLogin', '') != '':
-                request.session.set_expiry(KEEP_LOGIN_AGE)
+                request.session.set_expiry(age)
                 request.session.modified = True
         return response
     return _wrapped_view_func
