9d2e6632a703a973f71bc050efcf6d656ac3e25c
leeyongjoo <2yongjooo@gmail.com>
2021-10-17 17:01:24 +0900

    Style: Change ip and add secret key
    age 파라미터 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,9 +1,12 @@
+import os
+
 from .base import *
 
-ALLOWED_HOSTS = ['13.209.108.62', 'board.leeyongjoo.site']
+ALLOWED_HOSTS = ['3.35.91.242', 'board.leeyongjoo.site']
 
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
 ]
 
 DEBUG = False
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
